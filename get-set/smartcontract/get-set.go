1e41e941f30917dbdadea3f1bbb6817aa0227da3
jainarisht <arisht.jain@xooa.com>
2019-01-10 13:06:38 +0530

    f

--- get-set/smartcontract/get-set.go
+++ get-set/smartcontract/get-set.go
@@ -52,7 +52,7 @@ func (t *SimpleAsset) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
 	}
 	logger.Debug("id: ", id)
 
-	attrVal, found, err = cid.GetAttributeValue(stub, "ChannelId")
+	attrVal, found, err = cid.GetAttributeValue(stub, "IdentityName")
 	if err != nil {
 		return shim.Error("error getattrval")
 	}
